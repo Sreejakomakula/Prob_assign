5968aaa23f96e0cf5f520b98cea0b3195c4e9a31
Sreejakomakula <141927198+Sreejakomakula@users.noreply.github.com>
2023-08-22 22:37:49 +0530

    Add files via upload

---
+++ Prob_assign_4/codes/main.py
@@ -0,0 +1,6 @@
+def probability_of_53_sundays():
+    days_in_week = 7
+    return 1 / days_in_week
+
+prob = probability_of_53_sundays()
+print(f"Probability of getting 53 Sundays in a non-leap year: {prob:.2f}")
